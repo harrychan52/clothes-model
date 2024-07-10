f4f5e3933f09e637f7edb9751bf3546383e59d40
linying <linying@rayvision.com>
2024-07-10 16:06:32 +0800

    feat: add mirrors

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -7,7 +7,9 @@
 import { onMounted, onUnmounted } from "vue";
 import * as THREE from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Reflector } from "three/addons/objects/Reflector.js";
 
 let camera: THREE.PerspectiveCamera,
   scene: THREE.Scene,
@@ -26,94 +28,114 @@ onUnmounted(() => {
 });
 
 function init() {
-  const container = document.getElementById("container")!;
-
+  const container = document.getElementById("container");
   // 创建场景
   scene = new THREE.Scene();
-
+  scene.background = new THREE.Color(0x999999);
   // 设置相机
   camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+    1,
+    100
   );
-  //调整相机高度 45°俯视角
-  camera.position.y = 10;
-
-  // 设置渲染器
+  camera.position.y = -2;
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  container.appendChild(renderer.domElement);
+  container!.appendChild(renderer.domElement);
+  
+  const mainLight = new THREE.PointLight(0xffffff, 2.5, 250, 0);
+  mainLight.position.y = 60;
+  scene.add(mainLight);
 
-  scene.background = new THREE.Color(0xffc0cb);
+  const greenLight = new THREE.PointLight(0xffffff, 0.5, 1000, 0);
+  greenLight.position.set(550, 50, 0);
+  scene.add(greenLight);
 
-  // 添加光源
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(ambientLight);
-  // 添加点光源
-  const pointLight = new THREE.PointLight(0xffffff, 1, 100); // 颜色，强度，距离
-  pointLight.position.set(10, 10, 10); // 设置光源位置
-  scene.add(pointLight);
+  const redLight = new THREE.PointLight(0xffffff, 0.5, 1000, 0);
+  redLight.position.set(-550, 50, 0);
+  scene.add(redLight);
 
-  // 添加平行光
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-  directionalLight.position.set(0, 1, 0); // 设置光源方向
-  scene.add(directionalLight);
+  const blueLight = new THREE.PointLight(0xbbbbfe, 0.5, 1000, 0);
+  blueLight.position.set(0, 50, 550);
+  scene.add(blueLight);
 
-  // 加载.obj模型
-  const loader = new OBJLoader();
+  // 地面镜子
+  // const circleGeometry = new THREE.PlaneGeometry(12, 12);
+  // const groundMirror = new Reflector(circleGeometry, {
+  //   clipBias: 0.003,
+  //   textureWidth: window.innerWidth * window.devicePixelRatio,
+  //   textureHeight: window.innerHeight * window.devicePixelRatio,
+  //   color: 0xb5b5b5,
+  // });
+  // groundMirror.position.y = -6;
+  // groundMirror.rotateX(-Math.PI / 2);
+  // groundMirror.rotateZ(-Math.PI / 4);
+  // scene.add(groundMirror);
+
+  // 创建左边镜子
+  const geometry = new THREE.PlaneGeometry(20, 20);
+  const verticalLeftMirror = new Reflector(geometry, {
+    clipBias: 0.003,
+    textureWidth: window.innerWidth * window.devicePixelRatio,
+    textureHeight: window.innerHeight * window.devicePixelRatio,
+    color: 0xc1cbcb,
+  });
+  verticalLeftMirror.position.y = 2;
+  verticalLeftMirror.position.z = -8;
+  verticalLeftMirror.position.x = 6;
+  verticalLeftMirror.rotation.y = -Math.PI / 4;
+  scene.add(verticalLeftMirror);
+
+  // 创建右边镜子
+  const verticalRightMirror = new Reflector(geometry, {
+    clipBias: 0.003,
+    textureWidth: window.innerWidth * window.devicePixelRatio,
+    textureHeight: window.innerHeight * window.devicePixelRatio,
+    color: 0xc1cbcb,
+  });
+  verticalRightMirror.position.y = 2;
+  verticalRightMirror.position.z = -8;
+  verticalRightMirror.position.x = -6;
+  verticalRightMirror.rotation.y = Math.PI / 4;
+  scene.add(verticalRightMirror);
 
   // 加载人物模型
-  // loader.load("/src/assets/FinalBaseMesh.obj", (object) => {
-  loader.load("/src/assets/male.obj", (object) => {
-    // scene.add(object);
-    adjustColorToObject(object);
-    adjustCameraToObject(object);
-  });
-
+  const loader = new OBJLoader();
+  loader.load(
+    "/src/assets/male.obj",
+    (object) => {
+      adjustCameraToObject(object);
+      scene.add(object);
+    },
+    undefined,
+    function (error) {
+      console.error(error);
+    }
+  );
   // 加载衣物模型
-  const clothesModels = [
-    "/src/assets/shirt.blend",
-    "/src/assets/pant.obj",
-    "/src/assets/suit.obj",
-  ];
+  const clothesModels = ["/src/assets/t-shirt.glb"];
   clothesModels.forEach((modelPath) => {
-    loader.load(modelPath, (object) => {
-      // scene.add(object);
-      adjustCameraToObject(object, true);
-      adjustColorToObject(object, true);
-    });
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load(
+      modelPath,
+      (gltf) => {
+        const gltfScene = gltf.scene;
+        gltfScene.scale.set(5, 5, 5);
+        gltfScene.position.set(0, -7.7, 0.18);
+        scene.add(gltfScene);
+      },
+      undefined,
+      function (error) {
+        console.error(error);
+      }
+    );
   });
 
   // 设置控制器
   controls = new OrbitControls(camera, renderer.domElement);
 }
-const adjustColorToObject = (object, isClothe = false) => {
-  object.traverse((child) => {
-    if (child instanceof THREE.Mesh) {
-      // Check if the material has vertex colors
-      if (child.material.vertexColors !== undefined) {
-        child.material.color.setRGB(1, 1, 1); // 设置为白色，保持原本的颜色
-        child.material.vertexColors = THREE.NoColors; // 禁用顶点颜色
-      }
-    }
-  });
-  scene.add(object);
-};
-
-function adjustCameraToObject(object, isClothe = false) {
-  if (isClothe) {
-    const targetPoint = new THREE.Vector3(0, 1, 0);
-    // 设置衣服的朝向
-    object.lookAt(targetPoint);
-    // 衣服旋转
-    object.rotation.z = Math.PI;
-    object.scale.set(0.08, 0.08, 0.08);
-    object.position.z = -0.5;
-  } else {
-    // object.scale.set(0.9999, 0.9999, 0.9999);
-  }
+function adjustCameraToObject(object) {
   // 计算模型的边界框
   const box = new THREE.Box3().setFromObject(object);
   const center = box.getCenter(new THREE.Vector3());
@@ -137,7 +159,8 @@ function adjustCameraToObject(object, isClothe = false) {
 
 function animate() {
   requestAnimationFrame(animate);
-  controls.update(); // 只有在使用OrbitControls时才需要
+  controls.update();
+  // 只有在使用OrbitControls时才需要
   renderer.render(scene, camera);
 }
 </script>
